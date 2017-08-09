12a70956c24d367cd1d4a2c279f6a537eee7b4e6
iradkot <irad16@gmail.com>
2017-08-09 19:43:13 +0300

    adding functionality to event boxes

--- client/src/OpenEvents/Events.js
+++ client/src/OpenEvents/Events.js
@@ -1,42 +1,31 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import EventsBox from './EventsBox'
 class Events extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          
+
         }
     }
-    
+
     ///here we decide how to desplay the events:
     renderEvents() {
         // console.log(this.props);
-        return this.props.events.map((event, index) => <div className="col-md-6 event" key={index}>
-            <Link to={'../event-page/' +event._id}>
-                <img className="img-responsive" src={event.pic} />
-                <h3>{event.title}</h3></Link><img width="30" className="numPeople" src="images/numPeople.png"/><span className="numPeople">{event.participants.length}</span>
-                <div className="clearfix"></div>
-                
-                <p>{event.desc} </p>
-                <img className="prileImg eventProPic"  src={event.createdby.myPic} />{'    '}<span className="profileName">{event.createdby.name} Create the Event</span>
-                <p><i className="fa fa-clock-o" aria-hidden="true"></i> - {event.date.substring(0,10)} </p>
-                <p><img width="18" className="" src="images/location.png"/> - {event.location.city},{event.location.street}-{event.location.num} {'    '} <a href="#"> Get Direction</a></p>
-                <p><button className="btnJoin"> join</button> <button className="btnJoin facebook"> SHARE ON FACEBOOK</button></p>
-            
-        </div>);
+        return this.props.events.map((event, index) =><EventsBox key={index} event={event} index={index}/>);
     }
 
     render() {
-       
-            return (
 
-                <div className="row">
-                    {this.renderEvents()}
-                </div>
+        return (
 
-            );
-        
+            <div className="row">
+                {this.renderEvents()}
+            </div>
+
+        );
+
     }
 }
 export default Events;
---
+++ client/src/OpenEvents/EventsBox.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+class EventBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num_of_participants: this.props.event.participants.length,
+            participate: this.props.event.participants.indexOf(JSON.parse(localStorage.User).id) > -1? true: false
+        }
+    }
+    joinEvent(event) {
+        let user_id = JSON.parse(localStorage.User).id;
+        axios.post(`/joinEvent/${event._id}/${user_id}`)
+            .then(() => {
+                let num_of_participants = this.state.num_of_participants+1;
+                this.setState({ participate: true, num_of_participants: num_of_participants })
+            })
+    }
+
+    leaveEvent(event) {
+        let user_id = JSON.parse(localStorage.User).id;
+        axios.post(`/leaveEvent/${event._id}/${user_id}`)
+            .then(() => {
+                let num_of_participants = this.state.num_of_participants-1;
+                this.setState({ participate: false, num_of_participants: num_of_participants });
+            })
+    }
+
+
+    render() {
+        let { index,event } = this.props;
+        return (
+            <div className="col-md-6 event" key={index}>
+                <Link to={'../event-page/' + event._id}>
+                    <img className="img-responsive" src={event.pic} />
+                    <h3>{event.title}</h3></Link><img width="30" className="numPeople" src="images/numPeople.png" /><span className="numPeople">{this.state.num_of_participants}</span>
+                <div className="clearfix"></div>
+
+                <p>{event.desc} </p>
+                <img className="prileImg eventProPic" src={event.createdby.myPic} />{'    '}<span className="profileName">{event.createdby.name} Create the Event</span>
+                <p><i className="fa fa-clock-o" aria-hidden="true"></i> - {event.date.substring(0, 10)} </p>
+                <p><img width="18" className="" src="images/location.png" /> - {event.location.city},{event.location.street}-{event.location.num} {'    '} <a href="#"> Get Direction</a></p>
+                <p>
+                    <button className={this.state.participate? "noShow" : "btnJoin"} onClick={() => { this.joinEvent(event) }}> join</button>
+                    <button className={this.state.participate? "btnJoin" : "noShow"} onClick={() => { this.leaveEvent(event) }}> leave</button>
+                    <button className="btnJoin facebook"> SHARE ON FACEBOOK</button>
+                </p>
+
+            </div>
+
+        );
+
+    }
+}
+export default EventBox;
